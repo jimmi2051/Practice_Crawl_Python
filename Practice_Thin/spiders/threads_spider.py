5d39760f03669d095e67554dc77b4c2704f0326e
DefTnT <jimmi2051@gmail.com>
2019-05-09 23:39:11 +0700

    update

--- Practice_Thin/spiders/threads_spider.py
+++ Practice_Thin/spiders/threads_spider.py
@@ -27,21 +27,31 @@ class ThreadsSpider(scrapy.Spider):
                 if title.rstrip().strip().lower() == record.rstrip().strip().lower():
                     if result[countUrl][0] == "/":
                         result[countUrl] = "https://vnexpress.net"+result[countUrl]
-                    self.url_list.append(result[countUrl])
+                        for i in range(1,30):
+                            if i == 1:
+                                yield scrapy.Request(url=result[countUrl], callback=self.parse)
+                            yield scrapy.Request(url=result[countUrl]+"-p"+str(i), callback=self.parse)
+                    else:
+                        for i in range(1,30):
+                            if i == 1:
+                                yield scrapy.Request(url=result[countUrl], callback=self.parse)
+                            yield scrapy.Request(url=result[countUrl]+"/p"+str(i), callback=self.parse)
+                        # self.url_list.append(result[countUrl])
             countUrl=countUrl+1
-        for url in self.url_list:
-            for i in range(1,5):
-                if i == 1:
-                    yield scrapy.Request(url=url, callback=self.parse)
-                yield scrapy.Request(url=url+"-p"+str(i), callback=self.parse)
+        # for url in self.url_list:
+        #     for i in range(1,5):
+        #         if i == 1:
+        #             yield scrapy.Request(url=url, callback=self.parse)
+        #         yield scrapy.Request(url=url+"-p"+str(i), callback=self.parse)
                            
-        self.log('Saved file %s' % filename)
     def parse(self, response):
 
         page = response.url.split("/")
-
-        filename = page[len(page)-1] + ".html"
-
+        filename = ""
+        if page[len(page)-1][0]=="p":
+            filename = page[len(page)-2]+"-"+page[len(page)-1]+ ".html"
+        else:
+            filename = page[len(page)-1] + ".html"
         with open(self.folder_result+"/"+filename, 'wb') as f:
             f.write(response.body)
 
